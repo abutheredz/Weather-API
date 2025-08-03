6d50495e471f0d9fddf670e6e224a520c0018a12
abutheredz <75064476+abutheredz@users.noreply.github.com>
2025-08-03 20:35:46 +0800

    Update code.py

--- code.py
+++ code.py
@@ -21,3 +21,50 @@ def city_search(api_key,q):
     return response.json()
   else:
     return "Error"
+
+##Alternative if u want to print the actual result data
+#city_result1= city_search(API_KEY,"kuala lumpur")
+#print(city_result1)
+
+
+#2 - Current Conditions
+
+
+def get_current_condition(api_key,location_key):
+
+  endpoint = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}"
+  params = {
+      "apikey":api_key,
+
+  }
+  response =requests.get(endpoint,params = params)
+
+
+  if response.status_code == 200:
+    return response.json()
+  else:
+    print(f"Error: {response.status_code}")
+    return "Error"
+
+
+### 1 DAY OF DAILY FORECAST
+
+
+def get_daily_forecast(api_key,location_key):
+
+  endpoint = f"http://dataservice.accuweather.com/forecasts/v1/daily/1day/{location_key}"
+  params = {
+      "apikey":api_key,
+
+  }
+  response =requests.get(endpoint,params = params)
+
+
+  if response.status_code == 200:
+    return response.json()
+  else:
+    print(f"Error: {response.status_code}")
+    return "Error"
+
+
+
